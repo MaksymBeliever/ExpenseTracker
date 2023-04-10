896bd7eb6d5f2b8ae2af17d7f9486a852f188095
max <max.beliba@alternatestates.com>
2023-04-10 12:35:48 +0300

    Date formatting was changed and fixed button styles

--- components/ExpensesOutput.js
+++ components/ExpensesOutput.js
@@ -34,6 +34,36 @@ const DUMMY_EXPENSES = [
         amount: 18.59,
         date: new Date('2023-04-10')
     },
+    {
+        id: 'e6',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2022-12-19')
+    },
+    {
+        id: 'e7',
+        description: 'A pair of trousers',
+        amount: 89.29,
+        date: new Date('2023-01-03')
+    },
+    {
+        id: 'e8',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2023-02-14')
+    },
+    {
+        id: 'e9',
+        description: 'A book',
+        amount: 14.99,
+        date: new Date('2023-03-01')
+    },
+    {
+        id: 'e10',
+        description: 'Another book',
+        amount: 18.59,
+        date: new Date('2023-04-10')
+    },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
